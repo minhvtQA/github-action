bf5f167a8c9b3bb9590822a61ff0226456eee8f1
teq-minhvo <minhvo@teqnological.asia>
2026-06-16 10:46:44 +0700

    update test file and github action

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,30 @@
 import { test, expect } from "@playwright/test";
+import { allure } from "allure-playwright";
 
-test("has title", async ({ page }) => {
+test("has title", async ({ page }, testInfo) => {
   await page.goto("https://playwright.dev/");
-
+  await allure.attachment(
+    "homepage",
+    await page.screenshot({ fullPage: true }),
+    {
+      contentType: "image/png",
+    },
+  );
+  await testInfo.attach("homepage", {
+    body: await page.screenshot({ fullPage: true }),
+    contentType: "image/png",
+  });
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test("get started link", async ({ page }) => {
+test("get started link", async ({ page }, testInfo) => {
   await page.goto("https://playwright.dev/");
+  await page.screenshot({ fullPage: true });
+  await testInfo.attach("homepage", {
+    body: await page.screenshot({ fullPage: true }),
+    contentType: "image/png",
+  });
 
   // Click the get started link.
   await page.getByRole("link", { name: "Get started" }).click();
